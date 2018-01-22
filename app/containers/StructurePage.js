8d2bb32d7660e4d8c594dbeb751de913eb95d0e6
Amila Welihinda <amilajack@gmail.com>
2018-01-21 17:05:22 -0800

    Initial schema structure constraints changes

--- app/containers/StructurePage.js
+++ app/containers/StructurePage.js
@@ -2,40 +2,65 @@
 import React, { Component } from 'react';
 import ReactTable from 'react-table';
 import Select from 'react-select';
-
-const options = [
-  { value: 'BINARY', label: 'BINARY' },
-  { value: 'TEXT', label: 'TEXT' },
-  { value: 'NUMERIC', label: 'NUMERIC' },
-  { value: 'REAL', label: 'REAL' },
-  { value: 'BLOB', label: 'BLOB' }
-];
-
-const data = [{
-  name: 'username',
-  autoIncrement: 'false',
-  primaryKey: 'false',
-  defaultTypeValue: 'TEXT',
-  notNull: 'false',
-  unique: 'true',
-  default: 'foo',
-  checkConstraints: 'true',
-}, {
-  name: 'password',
-  autoIncrement: 'false',
-  primaryKey: 'false',
-  defaultTypeValue: 'TEXT',
-  notNull: 'false',
-  unique: 'true',
-  default: 'bar',
-  checkConstraints: 'true',
-}];
-
+import TagAutocomplete from 'react-tag-autocomplete';
 
 export default class StructurePage extends Component {
+  options = [
+    { value: 'BINARY', label: 'BINARY' },
+    { value: 'TEXT', label: 'TEXT' },
+    { value: 'NUMERIC', label: 'NUMERIC' },
+    { value: 'REAL', label: 'REAL' },
+    { value: 'BLOB', label: 'BLOB' }
+  ];
+
+  data = [{
+    name: 'username',
+    autoIncrement: 'false',
+    primaryKey: 'false',
+    defaultTypeValue: 'TEXT',
+    notNull: 'false',
+    unique: 'true',
+    default: 'foo',
+    checkConstraints: 'true',
+  }, {
+    name: 'password',
+    autoIncrement: 'false',
+    primaryKey: 'false',
+    defaultTypeValue: 'TEXT',
+    notNull: 'false',
+    unique: 'true',
+    default: 'bar',
+    checkConstraints: 'true',
+  }];
+
+  // state = {
+  //   values: this.data.map(column => column.defaultTypeValue)
+  // };
+
   state = {
-    values: data.map(column => column.defaultTypeValue)
+    values: this.data.map(column => column.defaultTypeValue),
+    tags: [
+      { id: 1, name: 'Auto Increment' },
+      { id: 2, name: 'Primary Key' }
+    ],
+    suggestions: [
+      { id: 3, name: 'Bananas' },
+      { id: 4, name: 'Mangos' },
+      { id: 5, name: 'Lemons' },
+      { id: 6, name: 'Apricots' }
+    ]
   };
+
+  handleDelete(i) {
+    const tags = this.state.tags.slice(0);
+    tags.splice(i, 1);
+    this.setState({ tags });
+  }
+
+  handleAddition(tag) {
+    const tags = [].concat(this.state.tags, tag);
+    this.setState({ tags });
+  }
 
   render() {
     const { values } = this.state;
@@ -58,7 +83,7 @@ export default class StructurePage extends Component {
                 values: updatedRowValues
               });
             }}
-            options={options}
+            options={this.options}
           />
         </span>
       )
@@ -68,17 +93,25 @@ export default class StructurePage extends Component {
     }, {
       accessor: 'notNull',
       Header: 'Constraints',
+      width: '200px',
+      Cell: (row) => (
+        <TagAutocomplete
+          tags={this.state.tags}
+          suggestions={this.state.suggestions}
+          handleDelete={this.handleDelete.bind(this)}
+          handleAddition={this.handleAddition.bind(this)}
+        />
+      )
     }];
 
     return (
       <div className="Structure col-offset-2">
         <ReactTable
-          data={data}
+          data={this.data}
           columns={columns}
           showPageJump={false}
-          minRows={data.length}
+          minRows={this.data.length}
           showPagination={false}
-
         />
       </div>
     );
