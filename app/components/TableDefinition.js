e1b9055126fda4a68428419b5e76071e1935240d
Amila Welihinda <amilajack@gmail.com>
2018-03-13 11:29:00 -0700

    Reverted to ace editor

--- app/components/TableDefinition.js
+++ app/components/TableDefinition.js
@@ -1,6 +1,12 @@
 // @flow
 import React, { Component } from 'react';
-import MonacoEditor from 'react-monaco-editor';
+import AceEditor from 'react-ace';
+import 'brace';
+import 'brace/mode/sql';
+import 'brace/snippets/sql';
+import 'brace/theme/xcode';
+import 'brace/ext/language_tools';
+import 'brace/ext/searchbox';
 
 type Props = {
   tableDefinition: string
@@ -9,31 +15,22 @@ type Props = {
 export default class TableDefinition extends Component<Props, {}> {
   props: Props;
 
-  editorDidMount(editor, monaco) {}
-
-  // onChange(newValue: string, e) {}
-
   render() {
-    const { tableDefinition } = this.props;
-    // Options for Monaco:
-    // https://microsoft.github.io/monaco-editor/api/modules/monaco.editor.html#editortype
-    const options = {
-      selectOnLineNumbers: true,
-      minimap: {
-        enabled: false
-      },
-      readOnly: true
-    };
     return (
-      <MonacoEditor
+      <AceEditor
+        focus
+        readOnly
+        enableBasicAutocompletion
+        enableSnippets
+        mode="sql"
+        theme="xcode"
+        name="querybox"
         width="100%"
-        height="300"
-        language="sql"
-        theme="vs"
-        value={tableDefinition}
-        options={options}
-        onChange={(a, b) => this.onChange(a, b)}
-        editorDidMount={(a, b) => this.editorDidMount(a, b)}
+        height="100%"
+        value={this.props.tableDefinition}
+        editorProps={{ $blockScrolling: Infinity }}
+        showPrintMargin={false}
+        enableLiveAutocompletion={false}
       />
     );
   }
