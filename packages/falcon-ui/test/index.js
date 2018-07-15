449455c45b54395eca4095e98d3bf7de3da88b3f
Amila Welihinda <amilajack@gmail.com>
2018-07-14 21:41:25 -0700

    Added initial boilerplate to get falcon-ui working

---
+++ packages/falcon-ui/test/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Button from '../src/components/Button';
+import '../src/styles/app.global.scss';
+
+class Root extends Component {
+  render() {
+    return <Button>Foobar</Button>
+  }
+}
+
+render(<Root />, document.querySelector('#root'));
