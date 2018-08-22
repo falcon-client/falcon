2fe3c15444dd6f3ee5ada6ce7591dc1f02d7fd3c
John Tran <jptran318@gmail.com>
2018-08-22 10:04:27 -0700

    falcon-ui: Added export of Tabs components

--- packages/falcon-ui/src/index.js
+++ packages/falcon-ui/src/index.js
@@ -1,2 +1,6 @@
 export Button from './components/Button';
-export Tabs from './components/Tabs';
+export Tabs from './components/Tabs/Tabs';
+export Tab from './components/Tabs/Tab';
+export TabList from './components/Tabs/TabList';
+export TabPanel from './components/Tabs/TabPanel';
+export HTMLTabs from './components/HTMLTabs';
